83c78e6dd7069335a6e71485fa2756b388c99a9e
chenzhaosheng <chenzhaosheng@baidu.com>
2024-11-13 16:31:37 +0800

    electron

---
+++ forge.config.js
@@ -0,0 +1,49 @@
+import { FusesPlugin } from'@electron-forge/plugin-fuses';
+import { FuseV1Options, FuseVersion } from '@electron/fuses';
+
+export default {
+  packagerConfig: {
+    asar: true,
+    // 指定目标架构
+    arch: process.arch === 'arm64' ? 'arm64' : 'x64',
+    // 或者您可以明确指定构建多个架构
+    // arch: ['x64', 'arm64'],
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+      // 指定目标架构
+      arch: ['x64', 'arm64'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
+
